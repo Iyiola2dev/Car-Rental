2064f14929a42961067cc97c1e08c7b73a18934c
Lexicon <iyiolad21@gmail.com>
2024-09-07 21:10:29 +0100

    final touch

--- src/components/DownloadSection.jsx
+++ src/components/DownloadSection.jsx
@@ -6,7 +6,7 @@ const DownloadSection = () => {
   return (
     <div className="flex flex-1 items-center px-[8rem] py-[5rem] gap-10 bg-black text-white ">
       <div className="flex flex-col gap-4">
-        <h3>Download our mobile app ⚡️</h3>
+        <h3 className="text-xl font-semibold" >Download our mobile app ⚡️</h3>
         <p className="w-[75%]">
           Get exclusive access to car rentals with our mobile app. Download now
           and experience convenience on the go.
@@ -16,7 +16,7 @@ const DownloadSection = () => {
         <button className="flex w-[193px] h-[55px] justify-center  items-center gap-2 bg-white text-black py-2 px-2 rounded-3xl">
           <IoLogoApple className=" text-[1.5rem]" />
           <span className="flex flex-col items-start">
-            <h5 className="text-[9px]">DOWNLOAD ON THE</h5>
+            <h5 className="text-[9px] ">DOWNLOAD ON THE</h5>
             <h6  className="text-sm">APP STORE</h6>
           </span>
         </button>
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import { BsDribbble, BsInstagram, BsTwitter, BsYoutube } from 'react-icons/bs'
 
 const Footer = () => {
   return (
-    <div>F</div>
+    <div className='flex justify-between items-center bg-black text-white px-[3rem] border-t-slate-50 border-t py-7'>
+        <img className='h-3' src="https://res.cloudinary.com/dtlejpoxq/image/upload/v1724925135/car-rentals/Heading_n3d73c.png" alt="" />
+        <ul className='flex justify-center items-center gap-9 '>
+            <li>Rent</li>
+            <li>Share</li>
+            <li>About us</li>
+            <li>Contact</li>
+        </ul>
+        <div className='flex justify-center items-center gap-3 '>
+            <button className='h-[25px] w-[25px]  px-1 py-2 rounded-full flex justify-center items-center  bg-gray-900 text-white'><BsInstagram className='text-[15px]'/></button>
+            <button className='h-[25px] w-[25px]  px-1 py-2 rounded-full flex justify-center items-center  bg-gray-900 text-white'><BsDribbble className='text-[15px]'/></button>
+            <button className='h-[25px] w-[25px]  px-1 py-2 rounded-full flex justify-center items-center  bg-gray-900 text-white'><BsTwitter className='text-[15px]'/></button>
+            <button className='h-[25px] w-[25px]  px-1 py-2 rounded-full flex justify-center items-center  bg-gray-900 text-white'><BsYoutube className='text-[15px]'/></button>
+        </div>
+    </div>
   )
 }
 
